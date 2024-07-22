cf0d2cc4aea5395fb8793d5d31e6568f51df1a15
Rana Badawy <ranasaber1996@gmail.com>
2024-07-22 11:44:31 -0500

    Modify README file

--- README.md
+++ README.md
@@ -1,17 +1,80 @@
-# Gen AI - LangChain - Query SQL DB
- 
-# AI-Powered Natural Language to SQL Query Processor
+# Natural Language to SQL Query with Streamlit and Docker
 
-This project demonstrates how to use AI to convert natural language questions into SQL queries and retrieve answers from a MySQL database. The solution leverages LangChain, Google Palm, and Chroma for few-shot learning and semantic similarity selection.
+This project provides an interface to convert natural language input into SQL queries and execute them against a MySQL database. The application uses Streamlit for the user interface and is containerized using Docker for ease of deployment.
 
 ## Features
 
-- Converts natural language questions into SQL queries.
-- Executes SQL queries on a MySQL database.
-- Rephrases SQL query results into user-friendly answers.
-- Utilizes few-shot learning for improved query generation.
-- Supports semantic similarity selection for example-based learning.
+- **Natural Language Processing**: Converts user input in natural language to SQL queries.
+- **MySQL Integration**: Executes the generated SQL queries against a MySQL database.
+- **Streamlit Interface**: Provides a web-based interface for user interaction.
+- **Dockerized**: Containerized with Docker for consistent and portable deployment.
 
-## Installation
+## Project Structure
+
+- `Dockerfile`: Docker configuration file to build the image.
+- `docker-compose.yml`: Docker Compose file to set up the application with necessary services.
+- `few_shots.py`: Script for few-shot learning setup.
+- `langchain_helper.py`: Helper functions for LangChain integration.
+- `main.py`: Main application file to run the Streamlit app.
+- `query_database_with_ai.ipynb`: Google Colab notebook containing the project code.
+
+## Technologies Used
+
+### AI and Natural Language Processing
+
+- **LangChain**: This project leverages LangChain for natural language processing. LangChain helps in converting natural language input into SQL queries. The integration is handled in the `langchain_helper.py` file, which contains helper functions to facilitate this conversion.
+- **Google PaLM**: The project uses Google PaLM as the large language model (LLM) to understand and process natural language input. PaLM is integrated through LangChain to generate accurate SQL queries from natural language queries.
+- **Chroma**: Chroma is used as the vector database for storing embeddings and facilitating similarity search. This helps in efficiently handling and retrieving relevant data based on the input queries.
+- **Few-Shot Learning**: The project employs few-shot learning techniques to improve the accuracy and relevance of the generated SQL queries. This is implemented in the `few_shots.py` file, where examples and contextual understanding are provided to fine-tune the model's responses.
+
+### Frontend
+
+- **Streamlit**: Streamlit is used for the user interface, providing a simple and interactive web-based interface for inputting natural language queries and displaying results.
+
+### Containerization
+
+- **Docker**: Docker is used to containerize the application, ensuring consistency across different environments and simplifying the deployment process. The configuration is provided in the `Dockerfile` and `docker-compose.yml`.
+
+## Getting Started
+
+### Prerequisites
+
+- Docker
+- Python
+
+### Installation
+
+1. **Clone the repository**
+
+   ```sh
+   git clone https://github.com/rana-badawy/Gen-AI_LangChain_Query_SQL_DB
+   ```
+
+2. **Add your Credentials**
+
+   Modify the `GOOGLE_API_KEY` in the `docker-compose.yml` file to include your key on Google Palm.
+
+   
+3. **Build and run the Docker container**   
+
+   ```sh
+   docker-compose up --build
+   ```
+4. **Create the database in Docker container**
+
+   Use the SQL script in `create_db_script.sql` to create the database in the docker container**
 
 
+5. **Access the Streamlit app**
+
+   Open your web browser and navigate to http://localhost:8501.
+
+## Usage
+
+1. **Input**: Enter your question in natural language in the input field.
+2. **Submit**: Click "Enter".
+3. **Output**: The generated SQL query and the result from the MySQL database will be displayed.
+
+## Notebook
+
+The project also includes a Google Colab notebook (`query_database_with_ai.ipynb`) that you can use to experiment with the code before running it locally.
--- database/create_db_script.sql
+++ database/create_db_script.sql
@@ -22,47 +22,94 @@ CREATE TABLE discounts (
     FOREIGN KEY (product_id) REFERENCES products(product_id)
 );
 
--- Create a stored procedure to populate the products table
-DELIMITER $$
-CREATE PROCEDURE PopulateClothes()
-BEGIN
-    DECLARE counter INT DEFAULT 0;
-    DECLARE max_records INT DEFAULT 100;
-    DECLARE product_type ENUM('T-shirt', 'Shirt', 'Blouse', 'Pants', 'Shorts', 'Skirt');
-    DECLARE brand ENUM('Levi', 'Nike', 'Adidas');
-    DECLARE color ENUM('Red', 'Blue', 'Black', 'White');
-    DECLARE size ENUM('XS', 'S', 'M', 'L', 'XL');
-    DECLARE price INT;
-    DECLARE stock INT;
+-- Insert data into the products table
+INSERT INTO products (product_id, product_type, brand, color, size, price, stock_quantity)
+VALUES
+	(1, 'T-shirt', 'Nike', 'Black', 'XS', 18, 66),
+    (2, 'Pants', 'Nike', 'Red', 'S', 23, 58),
+    (3, 'Shorts', 'Adidas', 'White', 'M', 21, 86),
+    (4, 'Blouse', 'Nike', 'White', 'XS', 20, 80),
+    (5, 'T-shirt', 'Levi', 'Red', 'XL', 45, 45),
+    (6, 'Blouse', 'Levi', 'Blue', 'L', 14, 61),
+    (7, 'Shorts', 'Nike', 'White', 'XL', 21, 85),
+    (8, 'T-shirt', 'Levi', 'Black', 'L', 13, 16),
+    (9, 'Shirt', 'Levi', 'Black', 'XL', 41, 93),
+    (10, 'Shorts', 'Levi', 'Black', 'L', 35, 26),
+    (11, 'Pants', 'Levi', 'Black', 'XL', 32, 95),
+    (12, 'Shirt', 'Levi', 'Blue', 'L', 38, 37),
+    (13, 'Blouse', 'Nike', 'Blue', 'L', 14, 70),
+    (14, 'T-shirt', 'Levi', 'Black', 'XS', 23, 39),
+    (15, 'Shirt', 'Nike', 'Blue', 'S', 16, 93),
+    (16, 'Skirt', 'Levi', 'Blue', 'XS', 35, 66),
+    (17, 'T-shirt', 'Levi', 'White', 'M', 46, 73),
+    (18, 'Shirt', 'Adidas', 'White', 'XS', 15, 43),
+    (19, 'Skirt', 'Nike', 'Black', 'S', 44, 19),
+    (20, 'Shorts', 'Levi', 'Black', 'XL', 42, 28),
+    (21, 'Blouse', 'Nike', 'Blue', 'S', 45, 29),
+    (22, 'Pants', 'Nike', 'White', 'S', 35, 48),
+    (23, 'T-shirt', 'Levi', 'Blue', 'XL', 14, 10),
+    (24, 'Blouse', 'Adidas', 'Blue', 'XL', 46, 64),
+    (25, 'Shorts', 'Levi', 'Red', 'S', 12, 46),
+    (26, 'Skirt', 'Adidas', 'Blue', 'M', 50, 34),
+    (27, 'Skirt', 'Nike', 'Black', 'L', 38, 25),
+    (28, 'Shirt', 'Adidas', 'Red', 'S', 29, 69),
+    (29, 'Blouse', 'Adidas', 'Blue', 'S', 48, 60),
+    (30, 'T-shirt', 'Adidas', 'White', 'XL', 31, 30),
+    (31, 'Skirt', 'Nike', 'Red', 'XS', 19, 68),
+    (32, 'Pants', 'Nike', 'Black', 'L', 15, 54),
+    (33, 'Shorts', 'Levi', 'Blue', 'L', 20, 38),
+    (35, 'Pants', 'Levi', 'White', 'XS', 37, 97),
+    (36, 'T-shirt', 'Adidas', 'Red', 'XS', 45, 36),
+    (37, 'Pants', 'Adidas', 'Red', 'XL', 33, 92),
+    (38, 'Shirt', 'Adidas', 'Black', 'S', 12, 34),
+    (39, 'Skirt', 'Levi', 'White', 'XS', 27, 13),
+    (40, 'Shirt', 'Adidas', 'White', 'XL', 30, 63),
+    (42, 'Pants', 'Adidas', 'Blue', 'S', 32, 69),
+    (43, 'Shirt', 'Nike', 'Red', 'S', 50, 11),
+    (44, 'Blouse', 'Levi', 'White', 'M', 23, 38),
+    (45, 'Skirt', 'Nike', 'White', 'S', 45, 68),
+    (46, 'Skirt', 'Nike', 'White', 'M', 37, 13),
+    (48, 'Pants', 'Levi', 'White', 'S', 41, 84),
+    (49, 'Blouse', 'Adidas', 'Red', 'XL', 20, 63),
+    (51, 'T-shirt', 'Levi', 'White', 'XL', 48, 19),
+    (52, 'Shirt', 'Adidas', 'Blue', 'L', 23, 77),
+    (53, 'Shirt', 'Adidas', 'Red', 'L', 33, 58),
+    (55, 'Shorts', 'Adidas', 'Blue', 'L', 39, 26),
+    (58, 'Shorts', 'Adidas', 'Red', 'XS', 33, 52),
+    (59, 'Pants', 'Nike', 'Blue', 'XS', 35, 63),
+    (60, 'Blouse', 'Levi', 'Red', 'S', 13, 60),
+    (61, 'Skirt', 'Nike', 'Blue', 'L', 14, 72),
+    (62, 'Pants', 'Levi', 'Blue', 'XL', 10, 14),
+    (65, 'T-shirt', 'Adidas', 'Black', 'XS', 22, 33),
+    (66, 'T-shirt', 'Nike', 'Blue', 'M', 44, 45),
+    (67, 'T-shirt', 'Nike', 'Black', 'L', 44, 46),
+    (68, 'Shirt', 'Nike', 'Blue', 'XL', 41, 42),
+    (69, 'Shirt', 'Nike', 'Black', 'S', 28, 62),
+    (70, 'T-shirt', 'Nike', 'Black', 'XL', 49, 89),
+    (71, 'Shorts', 'Nike', 'Black', 'XL', 31, 86),
+    (72, 'Skirt', 'Adidas', 'Blue', 'L', 11, 88),
+    (73, 'Pants', 'Levi', 'Red', 'M', 41, 16),
+    (74, 'T-shirt', 'Levi', 'Red', 'L', 20, 10),
+    (75, 'T-shirt', 'Levi', 'White', 'S', 33, 12),
+    (77, 'Pants', 'Nike', 'Black', 'XL', 41, 28),
+    (78, 'T-shirt', 'Adidas', 'Blue', 'L', 44, 45),
+    (79, 'Skirt', 'Nike', 'Red', 'M', 41, 38),
+    (81, 'Blouse', 'Levi', 'Black', 'M', 22, 38),
+    (84, 'Blouse', 'Levi', 'White', 'XL', 40, 22),
+    (85, 'Skirt', 'Nike', 'Blue', 'M', 35, 71),
+    (88, 'Pants', 'Nike', 'White', 'XS', 24, 66),
+    (89, 'Shirt', 'Levi', 'Red', 'M', 26, 59),
+    (90, 'Skirt', 'Nike', 'White', 'L', 38, 53),
+    (91, 'Shirt', 'Nike', 'Black', 'L', 22, 23),
+    (92, 'Shorts', 'Adidas', 'White', 'XL', 26, 47),
+    (93, 'T-shirt', 'Adidas', 'Black', 'L', 48, 60),
+    (96, 'T-shirt', 'Nike', 'Blue', 'S', 49, 10),
+    (97, 'Pants', 'Levi', 'Blue', 'XS', 24, 57),
+    (98, 'Shirt', 'Nike', 'Black', 'M', 42, 37),
+    (99, 'Pants', 'Levi', 'Black', 'XS', 18, 53),
+    (100, 'Blouse', 'Adidas', 'Black', 'XS', 36, 90);
 
-    -- Seed the random number generator
-    SET SESSION rand_seed1 = UNIX_TIMESTAMP();
-
-    WHILE counter < max_records DO
-        -- Generate random values
-        SET brand = ELT(FLOOR(1 + RAND() * 3), 'Levi', 'Nike', 'Adidas');
-        SET product_type = ELT(FLOOR(1 + RAND() * 6), 'T-shirt', 'Shirt', 'Blouse', 'Pants', 'Shorts', 'Skirt');
-        SET color = ELT(FLOOR(1 + RAND() * 4), 'Red', 'Blue', 'Black', 'White');
-        SET size = ELT(FLOOR(1 + RAND() * 5), 'XS', 'S', 'M', 'L', 'XL');
-        SET price = FLOOR(10 + RAND() * 41);
-        SET stock = FLOOR(10 + RAND() * 91);
-
-        -- Attempt to insert a new record
-        -- Duplicate type, brand, color, size combinations will be ignored due to the unique constraint
-        BEGIN
-            DECLARE CONTINUE HANDLER FOR 1062 BEGIN END;  -- Handle duplicate key error
-            INSERT INTO products (product_type, brand, color, size, price, stock_quantity)
-            VALUES (product_type, brand, color, size, price, stock);
-            SET counter = counter + 1;
-        END;
-    END WHILE;
-END$$
-DELIMITER ;
-
--- Call the stored procedure to populate the products table
-CALL PopulateClothes();
-
--- Insert at least 10 records into the discounts table
+-- Insert 10 records into the discounts table
 INSERT INTO discounts (product_id, pct_discount)
 VALUES
 (1, 10.00),
